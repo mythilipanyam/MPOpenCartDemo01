d99afb3e3e424a2e9db61c10f53b5f37b495ff6e
Chandra Panyam <mpanyam@gmail.com>
2021-08-30 12:22:53 +0100

    Dummy Page And DummyPageTest are created

---
+++ src/main/java/com/qa/opencart/pages/DummyPage.java
@@ -0,0 +1,9 @@
+package com.qa.opencart.pages;
+
+public class DummyPage {
+	
+	public void dummyPage() {
+		System.out.println("This is a dummy page");
+	}
+
+}
---
+++ src/test/java/com/qa/opencart/tests/DummyPageTest.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DummyPageTest {
+
+	
+	@Test
+	public void dummyPageTest() {
+		Assert.assertTrue(true);
+	}
+	
+}
