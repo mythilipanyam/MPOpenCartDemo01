9d9f33128a6c4348b661920e02bb6da49601db65
Chandra Panyam <mpanyam@gmail.com>
2021-08-30 12:45:20 +0100

    dummy page test created

---
+++ src/test/java/com/qa/opencart/tests/DummyPageTest.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class DummyPageTest {
+	
+	
+	@Test
+	public void DummyPageTest() {
+		System.out.println("This dummy page test");
+		
+	}
+
+}
